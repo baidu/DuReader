2867d921b905aa7e8de7d4c5c59054a7a5a03d27
liuyuan04 <liuyuan04@baidu.com>
2017-11-06 16:25:30 +0800

    Add copyright add LISENCE file
    Change-Id: Ic06ecce8c85530cb39657f80ca9421acd2aa5b23

--- paddle/run.py
+++ paddle/run.py
@@ -1,16 +1,21 @@
 # -*- coding:utf8 -*-
-###############################################################################
+# ==============================================================================
+# Copyright 2017 Baidu.com, Inc. All Rights Reserved
 #
-# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-###############################################################################
-"""
-This module implements the BiDAF algorithm described in
-https://arxiv.org/abs/1611.01603
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
-Authors: liuyuan(liuyuan04@baidu.com)
-Date: 2017/09/20 12:00:00
-"""
+
 import argparse
 import logging
 import os
